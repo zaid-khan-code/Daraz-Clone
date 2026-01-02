e2fb7faabe89bf05430e736e7a51b2fec05fd594
Zaid <zaidbinasif468@gmail.com>
2026-01-02 12:28:10 +0500

    signup modal UI completed

--- src/components/Auth.jsx
+++ src/components/Auth.jsx
@@ -11,25 +11,48 @@ export default function Auth() {
           <div className="w-[90%] h-max flex justify-end mt-[11px] mr-0 mb-[-45px] ml-0">
             <FaTimes className=" text-2xl" />
           </div>
-          <div className="text-[17px] flex justify-center font-medium items-center h-max gap-10 w-full mt-4 mb-9">
-            <button className="text-[#858b9c] cursor-pointer px-3">
-              Password
+          <div className="text-[17px] flex justify-center font-medium items-center h-max gap-10 w-full mt-4">
+            <button className="text-[#858b9c] cursor-default px-3">
+              Sign up
             </button>
-            <span className="text-[#6867679f] cursor-default"> | </span>
-            <button className="cursor-pointer px-3 ">Phone Number</button>
           </div>
           <form className="w-full flex flex-col gap-10 font-semibold text-sm justify-center items-center">
-            <div className="flex w-full justify-center items-center gap-[10px] mt-10">
-              <div className="rounded-sm text-[#4b4f5fd9] px-4 py-3 w-[20%] font-medium max-w-max border border-[#cbced5]">
-                🇵🇰+92
+            <div className="flex flex-col gap-[10px]">
+              <div className="flex w-full justify-center items-center gap-[10px] mt-10">
+                <div className="rounded-sm text-[#4b4f5fd9] px-4 py-3 w-[30%] font-medium max-w-max border border-[#cbced5]">
+                  🇵🇰+92
+                </div>
+                <input
+                  className="rounded-sm focus:outline-[#2f2ff0] placeholder:text-[#858b9c] px-4 py-3 w-[65%] border border-[#cbced5]"
+                  type="tel"
+                  placeholder="Enter your phone number "
+                  name="phoneNumber"
+                  id="phoneNumber"
+                />
               </div>
-              <input
-                className="rounded-sm focus:outline-[#2f2ff0] placeholder:text-[#858b9c] px-4 py-3 w-[60%] border border-[#cbced5]"
-                type="tel"
-                placeholder="Enter your phone number "
-                name="phoneNumber"
-                id="phoneNumber"
-              />
+              <div className="flex justify-center">
+                <div className="flex justify-center gap-[10px] w-[80%]">
+                  <input
+                    type="checkbox"
+                    name="checkbox"
+                    className=" h-5 w-5 accent-[#4f39f6] cursor-pointer hover:accent-[#4f39f6cc] active:accent-[#4f39f6e6]
+ "
+                    id="checkbox"
+                  />
+                  <p className="text-[#858b9c]">
+                    By creating and/or using your account, you agree to our
+                    <span className="text-[#1e71ff] cursor-pointer">
+                      {" "}
+                      Terms of Use
+                    </span>{" "}
+                    and{" "}
+                    <span className="text-[#1e71ff] cursor-pointer">
+                      Privacy Policy
+                    </span>
+                    .
+                  </p>
+                </div>
+              </div>
             </div>
             <div className="flex gap-2 flex-col justify-center w-[80%] items-center ">
               <button
@@ -46,8 +69,11 @@ export default function Auth() {
               </button>
               <div>
                 <p className="text-[#858b9c] font-medium">
-                  Don't have an account?
-                  <span className="text-[#2978ff] cursor-pointer"> Sign up</span>
+                  Already have an account?
+                  <span className="text-[#2978ff] cursor-pointer">
+                    {" "}
+                    Log in now
+                  </span>
                 </p>
               </div>
             </div>
