48255663dd03995fc42cb73fc9554baad33b8865
Zaid <zaidbinasif468@gmail.com>
2026-01-03 16:41:15 +0500

    another hero is undercontruction

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import './App.css'
+import Main from './components/Main'
 import Navbar from './components/Navbar'
 function App() {
  
@@ -6,7 +7,7 @@ function App() {
   return (
     <>
     <Navbar />
-   
+   <Main />
     </>
   )
 }
